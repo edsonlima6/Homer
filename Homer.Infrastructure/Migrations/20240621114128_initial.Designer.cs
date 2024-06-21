af8ca342e06a8395ff168ff816dae4b6e86a18ce
edson.feitosa <Edson.Feitosa@malvernpanalytical.com>
2024-06-21 13:44:24 +0100

    add ef core sqlite

--- Homer.Infrastructure/Migrations/20240621114128_initial.Designer.cs
+++ Homer.Infrastructure/Migrations/20240621114128_initial.Designer.cs
@@ -12,7 +12,9 @@ namespace Homer.Infrastructure.Migrations
 {
     [DbContext(typeof(HomerDbContext))]
     [Migration("20240621114128_initial")]
+#pragma warning disable CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     partial class initial
+#pragma warning restore CS8981 // The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
